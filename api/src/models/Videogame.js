704e0004863182a6f7f4978176afb13183c94fe9
Ezequiel Elcontar <ezequielelcontar0@gmail.com>
2024-02-14 00:11:35 -0300

    second commit

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,12 +1,42 @@
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+// Luego le inyectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
     name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
     },
+    released: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    background_image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    rating: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+    },
+    platforms: {
+        type: DataTypes.ENUM('Xbox One')
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    createdInDb: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+    }
   });
 };
